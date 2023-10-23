3fd7cabceaa25151123da85c919aad1d9ca84c39
AndreiBuikin <no4kit14@mail.ru>
2023-10-23 23:12:21 +0700

    Добавьте файлы проекта.

---
+++ PraktRab10.2/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PraktRab10._2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Canvas1_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            RotateTransform rotation = (sender as Canvas).RenderTransform as RotateTransform;
+            if (rotation != null)
+            {
+                (sender as Canvas).RenderTransform = new RotateTransform(
+                    rotation.Angle + 20,
+                    rotation.CenterX,
+                    rotation.CenterY
+                );
+            }
+        }
+    }
+}
